3ad5ee5800dfa9b4602cb91ff3ce6ee9b4872967
Casey Sawyers <caseyisop@yahoo.com>
2025-05-04 20:38:48 -0500

    change again

--- ece103_hw11_2025/external_data.cpp
+++ ece103_hw11_2025/external_data.cpp
@@ -4,11 +4,12 @@
 #include "external_data.h"
 
 char nickname_list[25][50] = {
-    "cManTheMoneyGuy", "Dr.Mali*", "KaboomIsAMONSTER",
+    "cManTheMoneyGuy", "Dr.Mali*", "KaboomIsAMONSTER", "Mr. Munchy", "Mr. T", "Mr. T2", "Mr. T3", "Mr. T4", "Mr. T5", "Mr. T6", "Mr. T7", "Mr. T8", "Mr. T9", "Mr. T10", "Mr. T11", "Mr. T12", "Mr. T13", "Mr. T14", "Mr. T15", "Mr. T16", "Mr. T17", "Mr. T18", "Mr. T19", "Mr. T20",
 //*** I suggest that you do not modify above ***
 
 
 
 //*** I suggest that you do not modify below ***
      // the last entry and all potential remaining entires are empty strings
+     
 };
